c1e23ad862411282377f8a814c2ce2ee67f476aa
JM <jmagali.dev@gmail.com>
2025-07-19 15:45:04 -0400

    Adjust playRound function, complete playGame function, merge humanScore and computerScore into a single array

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Global variable declaration
-let computerScore = 0;
-let humanScore = 0;
-
 function getComputerChoice() {
     // Assigns a random value from 0 - 2
     let index = Math.floor(Math.random() * 3);
@@ -15,25 +11,51 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice, scores) {
     // Losing cases
     if (
         (humanChoice.toLowerCase() === "rock" && computerChoice === "paper")
         || (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors")
         || (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock")
     ) {
-        computerScore++;
+        scores[0]++;
         alert("You lose! " + computerChoice[0].toUpperCase() + computerChoice.slice(1, computerChoice.length).toLowerCase() + " beats " + humanChoice[0].toUpperCase() + humanChoice.slice(1, humanChoice.length).toLowerCase());
     }
+    // Tie case
+    else if (humanChoice.toLowerCase() === computerChoice.toLowerCase()) {
+        alert("Tie! You both chose " + computerChoice[0].toUpperCase() + computerChoice.slice(1, computerChoice.length).toLowerCase() + "!");
+    }
+    // Winning cases
     else {
-        humanScore++;
+        scores[1]++;
         alert("You win! " + humanChoice[0].toUpperCase() + humanChoice.slice(1, humanChoice.length).toLowerCase() + " beats " + computerChoice[0].toUpperCase() + computerChoice.slice(1, computerChoice.length).toLowerCase());
+    }
+
+    return scores;
+}
+
+function playGame() {
+    let scores = [0, 0];
+
+    // To play five rounds
+    for (let i = 0; i < 5; i++) {
+        // To get the choices of the computer and user
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection, scores);
+    }
+
+    if (scores[0] > scores[1]) {
+        alert("The Computer Wins! Computer Score: " + scores[0] + "; Your Score: " + scores[1]);
+    }
+    else if (scores[0] === scores[1]) {
+        alert("Tie! Computer Score: " + scores[0] + "; Your Score: " + scores[1])
+    }
+    else {
+        alert("You Win! Computer Score: " + scores[0] + "; Your Score: " + scores[1]);   
     }
 }
 
-// To get the choices of the computer and user
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
 
