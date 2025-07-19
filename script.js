88ff0200193351ca260ddc5ebeb8d6cfb419e72f
JM <jmagali.dev@gmail.com>
2025-07-19 15:09:37 -0400

    Make the getComputerChoice function more efficient
    Substitute the switch case with accessing the array elements

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 function getComputerChoice() {
     // Assigns a random value from 0 - 2
     let index = Math.floor(Math.random() * 3);
-    let choice;
-    
-    // Assigns a choice based on the random index
-    switch (index) {
-        case 0:
-            choice = "rock";
-            break;
-        case 1:
-            choice = "paper";
-            break;
-        default:
-            choice = "scissors";
-            break;
-    }
+    let choice = ["rock", "paper", "scissors"];
 
-    return choice;
+    return choice[index];
 }
