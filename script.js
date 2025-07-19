30a50cbc18bc8457dafc79af3115642bf8957e2f
JM <jmagali.dev@gmail.com>
2025-07-19 15:25:05 -0400

    Complete playRound function

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function getHumanChoice() {
     let choice = prompt("Please enter your choice ('rock', 'paper', or 'scissors')")
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    // Losing cases
+    if (
+        (humanChoice.toLowerCase() === "rock" && computerChoice === "paper")
+        || (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors")
+        || (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock")
+    ) {
+        computerScore++;
+        alert("You lose! " + computerChoice[0].toUpperCase() + computerChoice.slice(1, computerChoice.length).toLowerCase() + " beats " + humanChoice[0].toUpperCase() + humanChoice.slice(1, humanChoice.length).toLowerCase());
+    }
+    else {
+        humanScore++;
+        alert("You win! " + humanChoice[0].toUpperCase() + humanChoice.slice(1, humanChoice.length).toLowerCase() + " beats " + computerChoice[0].toUpperCase() + computerChoice.slice(1, computerChoice.length).toLowerCase());
+    }
+}
+
+// To get the choices of the computer and user
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
